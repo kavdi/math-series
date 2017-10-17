facc7fcdc76eae230a77f5b5229f3bcc3946af59
kavdihodgson <kavdyjh@gmail.com>
2017-10-17 16:37:35 -0700

    all done

--- test_series.py
+++ test_series.py
@@ -3,10 +3,29 @@
 
 import pytest
 
-def test_fibonacci():
-    from series import fibonacci
-    assert fibonacci(5) == 5
 
-def test_lucas():
+FIB_PARAMS = [(2, 1), (3, 2), (4, 3), (5, 5), (6, 8), (7, 13)]
+
+
+LUC_PARAMS = [(2, 3), (3, 4), (4, 7), (5, 11), (6, 18), (7, 29)]
+
+
+SUM_PARAMS = [(2, 0, 1, 1), (3, 0, 1, 2), (4, 0, 1, 3), (5, 0, 1, 5), (6, 0, 1, 8), (7, 0, 1, 13), (2, 2, 1, 3), (3, 2, 1, 4), (4, 2, 1, 7), (5, 2, 1, 11), (6, 2, 1, 18), (7, 2, 1, 29)]
+
+
+@pytest.mark.parametrize('n, result', FIB_PARAMS)
+def test_fibonacci(n, result):
+    from series import fibonacci
+    assert fibonacci(n) == result
+
+
+@pytest.mark.parametrize('n, result', LUC_PARAMS)
+def test_lucas(n, result):
     from series import lucas
-    assert lucas(5) == 11
+    assert lucas(n) == result
+
+
+@pytest.mark.parametrize('n, a, b, result', SUM_PARAMS)
+def test_sum_series(n, a, b, result):
+    from series import sum_series
+    assert sum_series(n, a, b)  == result
