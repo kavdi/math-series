47adebed476d1872b261f73464049675bb8c8548
Mark Reynoso <mreynoso@spu.edu>
2017-10-17 15:26:08 -0700

    pytest not working

--- series.py
+++ series.py
@@ -1,16 +1,21 @@
 """This file contains fuctions to create fibonacci series and lucas numbers."""
 
 
-def fibonacci(n):
-    fib_list = [0, 1]
-    while len(fib_list) <= n:
-        last_fib = fib_list[len(fib_list) - 1]
-        sib_fib = fib_list[len(fib_list) - 2]
-        fib_list.append(last_fib + sib_fib)
-    return fib_list.pop()
+# def fibonacci(n):
+#     fib_list = [0, 1]
+#     while len(fib_list) <= n:
+#         last_fib = fib_list[len(fib_list) - 1]
+#         sib_fib = fib_list[len(fib_list) - 2]
+#         fib_list.append(last_fib + sib_fib)
+#     return fib_list.pop()
 
 
-# def lucas(n):
-
+def lucas(n):
+    luc_list = [2, 1]
+    while len(luc_list) <= n:
+        last_luc = luc_list[len(luc_list) - 1]
+        sib_luc = luc_list[len(luc_list) - 2]
+        luc_list.append(last_luc + sib_luc)
+    return luc_list.pop()
 
 # def sum_series(a, b):
--- test_series.py
+++ test_series.py
@@ -3,7 +3,10 @@
 
 import pytest
 
-def test_fibonacci():
-    from series import fibonacci
-    assert fibonacci(5) == 5
-    #assert len(fib_list) == n and fib_list[n-1] == (fib_list[n-2] + fib_list[n-3])
+# def test_fibonacci():
+#     from series import fibonacci
+#     assert fibonacci(5) == 5
+
+def test_lucas():
+    from series import lucas
+    assert lucas(5) == 11
